7486ef4863755ed3886b48f5c471c78cecbcd411
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-19 16:17:34 +0800

    delete w-full

--- gold-app/src/components/Accordion.js
+++ gold-app/src/components/Accordion.js
@@ -66,7 +66,7 @@ const Accordion = () => {
                 <Disclosure as="div" className="mt-2">
                 {({ open }) => (
                     <>
-                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white px-4 py-2 text-left text-sm font-bold text-black border border-gray-600 w-[668px] h-14">
+                    <Disclosure.Button className="flex justify-between rounded-lg bg-white px-4 py-2 text-left text-sm font-bold text-black border border-gray-600 w-[668px] h-14">
                         <span>Bagaimana jika terjadi kecelakaan?</span>
                         <ChevronUpIcon
                         className={`${
